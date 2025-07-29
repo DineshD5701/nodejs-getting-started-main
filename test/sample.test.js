1f907bfbee90d20168af92c9b9021052271fb4b8
DineshD5701 <dineshd3463@gmail.com>
2025-07-29 14:57:52 +0530

    test

---
+++ test/sample.test.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+
+describe('Basic Sanity Test', function () {
+  it('should pass when 1 + 1 equals 2', function () {
+    assert.strictEqual(1 + 1, 2);
+  });
+});
+
